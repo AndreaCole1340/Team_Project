782d12a858425c5595d719686eb1dc588f3c5752
Andrea Cole <L00115856@student.lyit.ie>
2017-03-02 16:40:59 +0000

    Add files via upload

---
+++ addProjectPane.java
@@ -0,0 +1,107 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class addProjectPane extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					addProjectPane frame = new addProjectPane();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public addProjectPane() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewProject = new JLabel("New Project");
+		lblNewProject.setFont(new Font("Serif", Font.BOLD, 25));
+		lblNewProject.setForeground(Color.blue);
+		lblNewProject.setBounds(101, 11, 153, 33);
+		contentPane.add(lblNewProject);
+		
+		JLabel lblNewLabel = new JLabel("Customer ID");
+		lblNewProject.setFont(new Font("Serif", Font.BOLD, 15));
+		lblNewProject.setForeground(Color.black);
+		lblNewLabel.setBounds(30, 72, 73, 21);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 72, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setFont(new Font("Serif", Font.BOLD, 15));
+		lblName.setForeground(Color.black);
+		lblName.setBounds(38, 117, 46, 14);
+		contentPane.add(lblName);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(139, 114, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblStartDate = new JLabel("Start Date");
+		lblStartDate.setFont(new Font("Serif", Font.BOLD, 15));
+		lblStartDate.setForeground(Color.black);
+		lblStartDate.setBounds(30, 161, 73, 21);
+		contentPane.add(lblStartDate);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(139, 161, 86, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnAddTasks = new JButton("Add Tasks");
+		lblNewProject.setFont(new Font("Serif", Font.BOLD, 20));
+		lblNewProject.setForeground(Color.blue);
+		btnAddTasks.setBounds(38, 193, 153, 37);
+		contentPane.add(btnAddTasks);
+		
+		JButton btnReturnTomenu = new JButton("Return to Menu");
+		lblNewProject.setFont(new Font("Serif", Font.BOLD, 20));
+		lblNewProject.setForeground(Color.blue);
+		btnReturnTomenu.setBounds(258, 193, 111, 37);
+		contentPane.add(btnReturnTomenu);
+		
+		JLabel label = new JLabel("");
+		Image images = new ImageIcon(this.getClass().getResource("/addTask.png")).getImage();
+		label.setIcon(new ImageIcon(images));
+		label.setBounds(258, 37, 154, 146);
+		contentPane.add(label);
+	}
+
+}
---
+++ addTaskPane.java
@@ -0,0 +1,102 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+
+public class addTaskPane extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					addTaskPane frame = new addTaskPane();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public addTaskPane() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 468, 311);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAddNewTask = new JLabel("Add New Task Details");
+		lblAddNewTask.setFont(new Font("Serif", Font.BOLD, 25));
+		lblAddNewTask.setForeground(Color.blue);
+		lblAddNewTask.setBounds(57, 11, 270, 42);
+		contentPane.add(lblAddNewTask);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setFont(new Font("Serif", Font.BOLD, 15));
+		lblNewLabel.setForeground(Color.black);
+		lblNewLabel.setBounds(24, 88, 46, 21);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Type");
+		lblNewLabel_1.setFont(new Font("Serif", Font.BOLD, 15));
+		lblNewLabel_1.setForeground(Color.black);
+		lblNewLabel_1.setBounds(24, 134, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblDuration = new JLabel("Duration");
+		lblDuration.setFont(new Font("Serif", Font.BOLD, 15));
+		lblDuration.setForeground(Color.black);
+		lblDuration.setBounds(24, 183, 77, 25);
+		contentPane.add(lblDuration);
+		
+		JLabel lblRole = new JLabel("Role");
+		lblRole.setFont(new Font("Serif", Font.BOLD, 15));
+		lblRole.setForeground(Color.black);
+		lblRole.setBounds(24, 219, 46, 19);
+		contentPane.add(lblRole);
+		
+		textField = new JTextField();
+		textField.setBounds(111, 88, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(111, 131, 86, 20);
+		contentPane.add(comboBox);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(111, 187, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(111, 218, 86, 20);
+		contentPane.add(comboBox_1);
+		
+		JLabel label = new JLabel("");
+		Image images = new ImageIcon(this.getClass().getResource("/addTask.png")).getImage();
+		label.setIcon(new ImageIcon(images));
+		label.setBounds(261, 88, 157, 161);
+		contentPane.add(label);
+	}
+}
